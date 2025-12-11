5769587f95c74629cef926e7da7f8b07766f8be3
olefile <mamorareolefile@gmail.com>
2025-12-11 07:51:16 +0200

    Workinig with Lists

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from python-exercises!")
+
+#Working with Strings and Lists:
+names = ["Olefile", "Antonio", "Bucks"]
+nums = [10, 20, 30, 40]
+
+print(nums[1:3])
+print(nums[:2])
+
+del nums[1] #deletes item at index 1.
+print(nums)
+
+# List Comprehension (Advanced but easy)
+squares = [x*x for x in range(7)]
+print(squares)
 
 
-if __name__ == "__main__":
-    main()
+#Dictionaries and Tuples:
+
+
